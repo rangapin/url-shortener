91e6a20c02c63901de5cf51a513f99488798ef29
rangapin <rangapin@gmx.com>
2023-11-30 14:36:45 +0100

    Tailwind set up and style added

--- resources/views/shortened.blade.php
+++ resources/views/shortened.blade.php
@@ -1,10 +1,53 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shortened URL</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
-<body>
-    <h1>Shortened URL:</h1>
-    <p><a target="_blank" href="{{ $shortenedUrl }}">{{ $shortenedUrl }}</a></p>
+
+<body class="bg-gradient-to-r from-purple-500 to-indigo-500 min-h-screen flex items-center justify-center">
+
+    <div class="max-w-md p-14 bg-white rounded-md shadow-md">
+
+        <h1 class="text-2xl font-semibold mb-6 text-gray-800 text-center">Shortened URL:</h1>
+
+        <div class="p-12 bg-gray-100 rounded-md mb-4">
+            <p class="text-center text-blue-500">
+                <a target="_blank" href="{{ $shortenedUrl }}" class="underline">{{ $shortenedUrl }}</a>
+            </p>
+        </div>
+
+        <div class="flex space-x-4">
+
+            <!-- Go Back Button -->
+            <button onclick="history.back()" class="bg-gray-500 text-white px-6 py-4 rounded-md hover:bg-gray-600 focus:outline-none focus:ring focus:border-gray-300">
+                Go Back
+            </button>
+
+            <!-- Copy URL Button -->
+            <button onclick="copyToClipboard('{{ $shortenedUrl }}')" class="bg-blue-500 text-white px-6 py-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">
+                Copy URL
+            </button>
+
+        </div>
+
+    </div>
+
+    <script>
+        function copyToClipboard(url) {
+            const input = document.createElement('input');
+            input.value = url;
+            document.body.appendChild(input);
+            input.select();
+            document.execCommand('copy');
+            document.body.removeChild(input);
+            alert('URL copied to clipboard!');
+        }
+    </script>
+
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,37 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Welcome to the URL Shortener</title>
-</head>
-<body>
-    <h1>Welcome to the URL Shortener</h1>
+<html lang="en">
 
-    <form action="/shorten" method="post">
-        @csrf
-        <label for="url">Enter URL:</label>
-        <input type="url" name="url" id="url" required>
-        <button type="submit">Shorten URL</button>
-    </form>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to the URL Shortener</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+</head>
+
+<body class="bg-gradient-to-r from-purple-500 to-indigo-500 min-h-screen flex items-center justify-center">
+
+    <div class="max-w-md p-12 bg-white rounded-md shadow-md">
+
+        <h1 class="text-2xl font-semibold mb-6 text-gray-800">Welcome to the URL Shortener</h1>
+
+        <form action="/shorten" method="post" class="space-y-4">
+
+            @csrf
+
+            <label for="url" class="block text-sm font-medium text-gray-600"></label>
+            <input type="url" name="url" id="url" class="w-full p-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" required>
+
+            <button type="submit" class="w-full bg-blue-500 text-white p-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">
+                Shorten URL
+            </button>
+
+        </form>
+
+    </div>
+
 </body>
+
 </html>
+
+
+
